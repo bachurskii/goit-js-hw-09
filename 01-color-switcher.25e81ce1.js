9bf180d670dc50496f1af1b450a94b68b1fe6d69
bachurskii <123319264+bachurskii@users.noreply.github.com>
2023-05-30 14:43:42 +0000

    Deploying to gh-pages from @ bachurskii/goit-js-hw-09@638615bb0626bde473140398327e21372b88e7f7 🚀

--- 01-color-switcher.25e81ce1.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector;setTimeout()}();
-//# sourceMappingURL=01-color-switcher.25e81ce1.js.map
--- 01-color-switcher.5023d39d.js
+++
@@ -1,2 +0,0 @@
-document.querySelector;setTimeout();
-//# sourceMappingURL=01-color-switcher.5023d39d.js.map
